08c091af7c9ded2b4c3796e0e098b24490864fb9
ismailtaher <ismail.taher@ymail.com>
2025-04-22 17:45:08 +0500

    add all routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
+import RequireAuth from "./features/auth/RequireAuth";
 
 function App() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}></Route>
+      {/* Public Routes */}
+      <Route index element={<Public />} />
+      <Route path="login" element={<Login />} />
+
+      {/* Protected Routes */}
+      <Route element={<RequireAuth />}>
+        <Route path="welcome" element={<Welcome />} />
+      </Route>
+    </Routes>
   );
 }
 
